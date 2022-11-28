ee50743182a29508ff15154add5555d400f95a1c
leonidbezrukov <leonelone@icloud.com>
2022-11-28 19:24:40 +0300

    9-module

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
 
-import { say } from '../src/cli.js';
-import { getRandomNumber, greeting, runGame } from '../src/index.js';
+import {say} from '../src/cli.js';
+import {getRandomNumber, greeting, runGame} from '../src/index.js';
 
 const BRAIN_GCD_WELCOME = 'Find the greatest common divisor of given numbers.';
 
 const prepareBrainGcdQuestion = () => {
-    let num1 = getRandomNumber(1, 50);
-    let num2 = getRandomNumber(1, 50);
+  let num1 = getRandomNumber(1, 50);
+  let num2 = getRandomNumber(1, 50);
 
-    const question = `${num1} ${num2}`;
+  const question = `${num1} ${num2}`;
 
-    while (num2) {
-        const res = num2;
-        num2 = num1 % num2;
-        num1 = res;
-    }
+  while (num2) {
+    const res = num2;
+    num2 = num1 % num2;
+    num1 = res;
+  }
 
-    return [question, num1.toString()];
+  return [question, num1.toString()];
 };
 
 const main = () => {
-    const userName = greeting();
-    say(BRAIN_GCD_WELCOME);
+  const userName = greeting();
+  say(BRAIN_GCD_WELCOME);
 
-    runGame(userName, prepareBrainGcdQuestion);
+  runGame(userName, prepareBrainGcdQuestion);
 };
 
 main();
